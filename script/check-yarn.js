a1c61be1ca1faa8a8f48956b8c434cec13fba0c7
Fancy <fancyboynet@gmail.com>
2019-08-09 17:26:34 +0800

    feat: remove the check-yarn command default

--- script/check-yarn.js
+++
@@ -1,7 +0,0 @@
-const { log, isYarn } = require('../lib/utils')
-const shell = require('shelljs')
-if (!isYarn()) {
-  log.error('Please use yarn for installing.')
-  shell.exit(1)
-}
-shell.exit(0)
