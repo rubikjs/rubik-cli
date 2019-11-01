31e11b33c7ec08ca95c17784369a9436c92ec52e
Fancy <fancyboynet@gmail.com>
2019-11-01 17:14:26 +0800

    feat(a): b
    c
    
    d

--- command/commit.js
+++ command/commit.js
@@ -1,16 +1,26 @@
 'use strict'
 
-const TakeVersionCommand = require('../lib/take-version-command')
+const RubikCommand = require('../lib/rubik-command')
 // const { srcDir, eslintCLIEngineConfig } = require('../config')
 const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap
 const path = require('path')
 
-class CommitCommand extends TakeVersionCommand {
+class CommitCommand extends RubikCommand {
   constructor (rawArgv) {
     super(rawArgv)
   }
 
   async run () {
+    // https://github.com/commitizen/cz-cli/issues/667
+    Object.assign(process.env, {
+      CZ_TYPE: ' ',
+      CZ_SCOPE: ' ',
+      CZ_SUBJECT: ' ',
+      CZ_BODY: ' ',
+      CZ_ISSUES: ' ',
+      CZ_MAX_HEADER_WIDTH: 100,
+      CZ_MAX_LINE_WIDTH: 100
+    })
     bootstrap({
       cliPath: path.join(__dirname, '../node_modules/commitizen'),
       config: {
