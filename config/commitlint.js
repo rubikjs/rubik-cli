68b505c408373c223c4f62e23aa9321d7112eb2a
Fancy <fancyboynet@gmail.com>
2019-11-08 15:43:29 +0800

    fix(commitlint): fix path error

--- config/commitlint.js
+++ config/commitlint.js
@@ -1,3 +1,1 @@
-const path = require('path')
-const configPath = path.relative(process.cwd(), path.resolve(__dirname, '../node_modules/@commitlint/config-conventional'))
-module.exports = { extends: [configPath] }
+module.exports = { extends: ['@commitlint/config-conventional'] }
