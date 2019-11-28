33ae9ef259780bf0f3fbf6d16289c9d23495709b
Fancy <fancyboynet@gmail.com>
2019-11-28 14:02:17 +0800

    refactor(stylelint): only lint the necessary files when re-compile

--- webpack/stylelint.plugin.js
+++ webpack/stylelint.plugin.js
@@ -1,31 +1,83 @@
-const { stylelint } = require('../lib/utils')
+const { stylelint, isValidStylelintFile, debug } = require('../lib/utils')
+const { rootDir } = require('../config')
 const pluginName = 'StylelintWebpackPlugin'
-let errorOutput = ''
-async function lint (compilation, callback) {
-  const result = await stylelint({
-    fix: true
-  })
-  if (result && result.errored) {
-    errorOutput = result.output
-  } else {
-    errorOutput = ''
-  }
-  callback()
-}
 class StylelintWebpackPlugin {
+  constructor () {
+    this.errorOutput = ''
+    this.startTime = Date.now()
+    this.prevTimestamps = {}
+    this.isFirstRun = true
+    this.lastErrorFiles = []
+  }
+
   apply (compiler) {
-    compiler.hooks.run.tapAsync(pluginName, (compilation, callback) => {
-      return lint(compilation, callback)
-    })
     compiler.hooks.watchRun.tapAsync(pluginName, (compilation, callback) => {
-      return lint(compilation, callback)
+      return this.lint(compilation, callback)
     })
     compiler.hooks.afterCompile.tapAsync(pluginName, (compilation, next) => {
-      if (errorOutput) {
-        compilation.errors.push(new Error(errorOutput))
+      if (this.errorOutput) {
+        compilation.errors.push(new Error(this.errorOutput))
       }
       next()
     })
+  }
+
+  async lint (compilation, callback) {
+    const lintOpt = {
+      fix: true
+    }
+    if (!this.isFirstRun) {
+      lintOpt.files = this.getLintFiles(compilation)
+      lintOpt.globbyOptions = {
+        cwd: rootDir
+      }
+    }
+    this.prevTimestamps = compilation.fileTimestamps
+    const result = await stylelint(lintOpt)
+    if (result && result.errored) {
+      this.errorOutput = result.output
+    } else {
+      this.errorOutput = ''
+    }
+    if (result.results) {
+      this.updateLastErrorFiles(result.results)
+    }
+    if (this.isFirstRun) {
+      this.isFirstRun = false
+    }
+    callback()
+  }
+
+  getLintFiles (compilation) {
+    const changeFiles = this.getValidChangedFiles(compilation)
+    const files = this.lastErrorFiles.concat(changeFiles.filter(x => !this.lastErrorFiles.includes(x)))
+    debug('lintFiles', files)
+    return files
+  }
+
+  updateLastErrorFiles (results) {
+    const errorFiles = results.filter(x => x.errored).map(x => x.source)
+    const successFiles = results.filter(x => !x.errored).map(x => x.source)
+    this.lastErrorFiles = this.lastErrorFiles.concat(errorFiles.filter(x => !this.lastErrorFiles.includes(x)))
+    this.lastErrorFiles = this.lastErrorFiles.filter(x => !successFiles.includes(x))
+  }
+
+  getValidChangedFiles (compilation) {
+    const files = []
+    if (!compilation.fileTimestamps.size) {
+      return files
+    }
+    for (const [filename, timestamp] of compilation.fileTimestamps.entries()) {
+      if (isValidStylelintFile(filename) && this.hasFileChanged(filename, timestamp)) {
+        files.push(filename)
+      }
+    }
+    return files
+  }
+
+  hasFileChanged (filename, timestamp) {
+    const prevTimestamp = this.prevTimestamps.get(filename)
+    return (prevTimestamp || this.startTime) < (timestamp || Infinity)
   }
 }
 
